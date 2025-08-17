a8e4f24b1bb6342e7716a30e7a8308529ef231a1
Magic <Roger@rex42.com>
2025-08-17 18:15:38 -0500

    asdf

--- MachineTierSystem.swift
+++
@@ -1,9 +0,0 @@
-// MARK: - Machine Tier System
-struct MachineTier: Identifiable, Codable, Hashable {
-    let id: String
-    let name: String
-    let category: String
-    let speed: Double
-    let iconAsset: String?
-    let moduleSlots: Int
-}
--- RecipeModel.swift
+++
@@ -1,9 +0,0 @@
-// MARK: - Recipe Model
-struct Recipe: Identifiable, Codable, Hashable {
-    var id: String
-    var name: String
-    var category: String
-    var time: Double
-    var inputs: [String: Double]
-    var outputs: [String: Double]
-}
