82caa7c66fe4955c2f190ff9a4930823509c495b
Magic <Roger@rex42.com>
2025-08-17 14:51:31 -0500

    Initial Commit

---
+++ Factorio PlannerTests/Factorio_PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Factorio_PlannerTests.swift
+//  Factorio PlannerTests
+//
+//  Created by Daniel on 8/13/25.
+//
+
+import Testing
+@testable import Factorio_Planner
+
+struct Factorio_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
